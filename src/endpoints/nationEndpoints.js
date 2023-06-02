35bc600b8705e0de971213830faa05e31cbe81a5
rachelvaughn1998 <117373186+rachelvaughn1998@users.noreply.github.com>
2023-06-02 21:35:38 +0200

    Update nationEndpoints.js

--- src/endpoints/nationEndpoints.js
+++ src/endpoints/nationEndpoints.js
@@ -1,50 +1,42 @@
 import express from "express";
 import NationModel from "../models/nations.js";
-import cloudinary from "cloudinary";
+import upload from "../middleware/upload.js";
+import uploadToCloudinary from "../models/cloudinary.js";
+import cron from "node-cron";
 
 const nationEndpoints = express.Router();
 
-// configure Cloudinary with your API credentials
-cloudinary.config({
-  cloud_name: "dg4jye9k4",
-  api_key: "763628149477897",
-  api_secret: "V6ykTSqs7VPwU03gVEPyQDJhdfw",
-});
+nationEndpoints.post(
+  "/menu/:id",
+  upload.single("nationMenu"),
+  async (req, res) => {
+    try {
+      const data = await uploadToCloudinary(req.file.path, "Kandidat_menu");
 
-nationEndpoints.post("/:nationId/image", async (req, res) => {
-  try {
-    const nationId = req.params.nationId;
-    const fileStr = req.body.data;
- 
-    const uploadedImage = await cloudinary.uploader.upload(fileStr, {
-      folder: "menu", 
-      overwrite: true,
-      transformation: { width: 400, height: 400, crop: "limit" }, 
-    });
-  
-    const updatedNation = await NationModel.findByIdAndUpdate(
-      nationId,
-      {
-        "menu.url": uploadedImage.url,
-        "menu.public_id": uploadedImage.public_id,
-      },
-      { new: true }
-    );
-    res.status(200).json(updatedNation);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Server error" });
+      const menu = {
+        menuUrl: data.url,
+        publicId: data.public_id,
+      };
+
+      const updatedNation = await NationModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: menu },
+        { new: true }
+      );
+
+      res.status(200).send("Nation menu uploaded successfully");
+    } catch (error) {
+      res.status(400).send(error);
+    }
   }
-});
+);
 
 nationEndpoints.get("/getNations", (req, res) => {
   NationModel.find({})
     .then((result) => {
-      console.log("getresult", result);
       res.status(200).json(result);
     })
     .catch((err) => {
-      console.log("error", err);
       res.status(400).send({ error: "Could not get nations 🙁" });
     });
 });
@@ -57,7 +49,7 @@ nationEndpoints.post("/createNations", (req, res) => {
   const newNation = new NationModel(nation);
   newNation
     .save()
-    .then((result) => console.log("result", result))
+    .then(result)
     .catch((err) => {
       res.status(400).send({ error: "Could not create nations 🙁" });
     });
@@ -75,9 +67,9 @@ nationEndpoints.get("/:id", (req, res) => {
 
 nationEndpoints.post("/:id", (req, res) => {
   const nation = req.body;
-  console.log("nation", nation);
+
   var query = { id: req.params.id };
-  console.log("req.newData", req.newData);
+
   req.newData.guestCount = nation.guestCount;
 });
 
@@ -85,46 +77,79 @@ nationEndpoints.patch("/:id", (req, res) => {
   const { id } = req.params;
   const { maxCapacity, guestChange, description, image, header } = req.body;
 
-  if (!maxCapacity && !guestChange && !description && !image && !header) {
+  if (!maxCapacity && !guestChange && !description && !image) {
     res.status(400).send({ error: "Something is missing. Try again! 🙁" });
   }
 
-  let updateObj = {};
-  if (maxCapacity) {
-    updateObj.maxCapacity = maxCapacity;
-  }
-
-  if (description) {
-    updateObj.description = description;
-  }
-
-  if (guestChange === "add") {
-    updateObj.$inc = { guestCount: 1 };
-  }
-
-  if (guestChange === "remove") {
-    updateObj.$inc = { guestCount: -1 };
-  }
-
-  if (image) {
-    updateObj.image = image;
-  }
-
-  if (header) {
-    updateObj.header = header;
-  }
-
-  NationModel.findByIdAndUpdate(id, updateObj, { new: true })
-    .then((updatedNation) => {
-      if (!updatedNation) {
-        res.status(404).send({ error: "Nation not found 🙁" });
-      } else {
-        res.json(updatedNation);
+  NationModel.findById(id)
+    .then((nation) => {
+      if (!nation) {
+        throw new Error("Nation not foundd 🙁");
       }
+
+      const currentGuestCount = nation.guestCount || 0;
+      let updateObj = {};
+
+      if (maxCapacity) {
+        updateObj.maxCapacity = maxCapacity;
+      }
+
+      if (description) {
+        updateObj.description = description;
+      }
+
+      if (guestChange === "add") {
+        if (currentGuestCount < (maxCapacity || Infinity)) {
+          updateObj.$inc = { guestCount: 1 };
+        }
+      }
+
+      if (guestChange === "remove") {
+        if (currentGuestCount > 0) {
+          updateObj.$inc = { guestCount: -1 };
+        }
+      }
+
+      if (image) {
+        updateObj.image = image;
+      }
+
+      if (header) {
+        updateObj.header = header;
+      }
+
+      NationModel.findByIdAndUpdate(id, updateObj, { new: true })
+        .then((updatedNation) => {
+          if (!updatedNation) {
+            res.status(404).send({ error: "Nation not found 🙁" });
+          } else {
+            res.json(updatedNation);
+          }
+        })
+        .catch((err) => {
+          res.status(400).send({ error: "Could not update nation 🙁" });
+        });
     })
     .catch((err) => {
-      res.status(400).send({ error: "Could not update nation 🙁" });
+      res.status(404).send({ error: err.message });
     });
+});
+
+async function resetCounts() {
+  try {
+    const update = {
+      $set: {
+        guestCount: 0,
+        maxCapacity: 0,
+      },
+    };
+
+    await NationModel.updateMany({}, update);
+  } catch (error) {}
+}
+
+cron.schedule("0 4 * * *", () => {
+  resetCounts();
 });
 
 export default nationEndpoints;
